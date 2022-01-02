14b65ce509678fac07e54d891803cd9fdfe0866d
Israel <israeladefidipe@gmail.com>
2022-01-02 17:20:39 +0100

    fix: fixed accordion issue

--- src/components/style/FAQ.style.js
+++ src/components/style/FAQ.style.js
@@ -1,71 +1,60 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StyledFAQ = styled.section`
-    padding: 100px 0;
-
+	padding: 100px 0;
 `
 export const FAQContainer = styled.div`
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    
-    
+	margin: 0 auto;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
 
-    
-    width: 50%;
-    padding: 20px;
-    border-top: 1px solid ${ ({theme}) => theme.colors.neuBlue} ;
+	width: 50%;
+	padding: 20px;
+	border-top: 1px solid ${({ theme }) => theme.colors.neuBlue};
 
-    & h4{
-        color: ${ ({theme}) => theme.colors.neuDarkBlue};
-    }
+	& h4 {
+		color: ${({ theme }) => theme.colors.neuDarkBlue};
+	}
 
-    & p {
-        font-size: 1rem;
-        line-height: 36px;
-        letter-spacing: 0.15px;
-    }
+	& p {
+		font-size: 1rem;
+		line-height: 36px;
+		letter-spacing: 0.15px;
+	}
 
-    
+	&:last-of-type {
+		border-bottom: 1px solid ${({ theme }) => theme.colors.neuBlue};
+	}
 
-    &:last-of-type{
-        border-bottom: 1px solid ${ ({theme}) => theme.colors.neuBlue} ; 
-    }
+	&:hover {
+		h4 {
+			color: ${({ theme }) => theme.colors.pryRed};
+		}
+	}
 
-    &:hover{
-        h4{
-            color: ${ ({theme}) => theme.colors.pryRed};
-
-        }
-    }
-
-    @media (max-width: ${ ({theme}) => theme.queries.deskMini}){
-        width: 70% ;
-
-    }
-    @media (max-width: ${ ({theme}) => theme.queries.mobileMini}){
-        width: 100% ;
-
-    }
-    
+	@media (max-width: ${({ theme }) => theme.queries.deskMini}) {
+		width: 70%;
+	}
+	@media (max-width: ${({ theme }) => theme.queries.mobileMini}) {
+		width: 100%;
+	}
 `
 
-export const FAQuestion = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 10px 0;
-gap: 20px;
+export const FAQuestion = styled.button`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 10px 0;
+	gap: 20px;
 `
 
 export const MainContainer = styled.div`
-    margin-top: 50px; 
-    
+	margin-top: 50px;
 `
 
 export const ButtonContainer = styled.div`
-    display: grid;
-    place-items: center;
-    margin-top: 50px;
+	display: grid;
+	place-items: center;
+	margin-top: 50px;
 `
